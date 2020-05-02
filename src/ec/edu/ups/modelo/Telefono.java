54302245938282869a81660ba72bef38d2bb0747
OrlandoR54 <OrlandoR54@users.noreply.github.com>
2020-05-01 21:03:28 -0500

    Update Telefono.java

--- src/ec/edu/ups/modelo/Telefono.java
+++ src/ec/edu/ups/modelo/Telefono.java
@@ -1,5 +1,67 @@
 package ec.edu.ups.modelo;
 
-public class Telefono {
+import java.io.Serializable;
+
+public class Telefono implements Serializable {
+
+	/**
+	 * Atributo necesario para el envío de objetos a través de una comunicación, en
+	 * este caso hacia la base de datos
+	 */
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private String numero;
+	private String tipo;
+	private String operadora;
+
+	public Telefono() {
+		// Constructor obligatorio
+	}
+
+	public Telefono(int id, String numero, String tipo, String operadora) {
+		super();
+		this.id = id;
+		this.numero = numero;
+		this.tipo = tipo;
+		this.operadora = operadora;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getOperadora() {
+		return operadora;
+	}
+
+	public void setOperadora(String operadora) {
+		this.operadora = operadora;
+	}
+
+	// Método toString que permite describir a un objeto
+	@Override
+	public String toString() {
+		return "Telefono [id=" + id + ", numero=" + numero + ", tipo=" + tipo + ", operadora=" + operadora + "]";
+	}
 
 }
