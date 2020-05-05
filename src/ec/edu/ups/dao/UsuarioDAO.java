2f60e7a2bcf02af07d2d29f4281721111a59c72c
OrlandoR54 <OrlandoR54@users.noreply.github.com>
2020-05-04 19:03:02 -0500

    Create UsuarioDAO.java

---
+++ src/ec/edu/ups/dao/UsuarioDAO.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package ec.edu.ups.dao;
+
+import ec.edu.ups.modelo.Usuario;
+
+/**
+ * 
+ * Interface UserDAO.
+ * 
+ * 
+ * En esta interface se pueden agregar método específicos para el manejo
+ * del objeto User, por ejemplo: buscarUsuarioPorName,
+ * buscarUsuarioPorCedula, entre otras.
+ * 
+ * @author Orlando Real
+ *
+ *
+ */
+public interface UsuarioDAO extends GenericDAO<Usuario, String>{
+
+}
