b7e01b1a578436860658057324c95235014262ed
OrlandoR54 <OrlandoR54@users.noreply.github.com>
2020-05-04 19:03:09 -0500

    Create JDBCGenericDAO.java

---
+++ src/ec/edu/ups/mysql/jdbc/JDBCGenericDAO.java
@@ -0,0 +1,26 @@
+package ec.edu.ups.mysql.jdbc;
+
+import ec.edu.ups.dao.GenericDAO;
+
+
+/**
+ * 
+ * La clase abstracta JDBCGenericDAO implementa los métodos de la clase
+ * GenericDAO. Teniendo de esta manera una clase generica abstracta que será
+ * implementada por cada clase específica y permitirá persistir la información
+ * en la base de datos.
+ * 
+ * Así como también, en esta clase abstracta se obtiene dos conexiones a la base
+ * de datos para poder persistir la información.
+ * 
+ * @author Orlando Real
+ *
+ * @param <T>  la entidad o modelo que será persistida en la base de datos
+ * @param <ID> el id o llave primaria por la cuál se identificara al modelo como
+ *             valor único
+ */
+public abstract class JDBCGenericDAO<T, ID> implements GenericDAO<T, ID> {
+
+	protected Conexion conexionUno = Conexion.getJDBC1();
+	protected Conexion conexionDos = Conexion.getJDBC2();
+}
