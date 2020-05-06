4744843adee4db9fbd1f119d50f433f6bb9ad283
OrlandoR54 <OrlandoR54@users.noreply.github.com>
2020-05-06 14:41:04 -0500

    Update JDBCTelefonoDAO.java

--- src/ec/edu/ups/mysql/jdbc/JDBCTelefonoDAO.java
+++ src/ec/edu/ups/mysql/jdbc/JDBCTelefonoDAO.java
@@ -6,13 +6,19 @@ package ec.edu.ups.mysql.jdbc;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import ec.edu.ups.dao.TelefonoDAO;
 import ec.edu.ups.modelo.Telefono;
+import ec.edu.ups.modelo.Usuario;
 
 
+/**
+ * @author Orlando Real
+ *
+ */
 /**
  * @author Orlando Real
  *
@@ -25,71 +31,34 @@ public class JDBCTelefonoDAO extends JDBCGenericDAO<Telefono, String> implements
 		
 	}
 
+	/***
+	 * Se inserta el telefono con la clave foranea incluida, la cual es el numero de cedula del usuario
+	 */
 	@Override
 	public void create(Telefono telefono) {
 		// TODO Auto-generated method stub
-		conexionDos.update("INSERT Telefono VALUES (" + product.getId() + ", " + product.getAmount() + ", '"
-				+ telefono.getDescription() + "', " + product.getShoppingBasket().getId() + ")");
+		conexionUno.update("INSERT INTO Telefono  ('tel_numero', 'tel_tipo', 'tel_operadora', 'usu_cedula')" + 
+				"VALUES ( '" + telefono.getNumero() + "', '"+ telefono.getTipo() + "', '" + telefono.getOperadora() +"','"+telefono.getUsuario().getCedula() + "');");
 	}
 
 	@Override
-	public void update(Telefono telefono) {
-		// TODO Auto-generated method stub
-		conexionUno.update("UPDATE Telefono SET amount = " + product.getAmount() + ", description = '"
-				+ telefono.getDescription() + "' WHERE id = " + telefono.getId());
-	}
-
-	@Override
-	public void delete(Telefono telefono) {
-		// TODO Auto-generated method stub
-		conexionUno.update("DELETE FROM Telefono WHERE id = " + telefono.getId());
-	}
-
-	@Override
-	public List<Telefono> find() {
-		// TODO Auto-generated method stub
-		List<Telefono> list = new ArrayList<Telefono>();
-		ResultSet rsTelefono = conexionUno.query("SELECT * FROM Product");
-		try {
-			while (rsTelefono.next()) {
-				Telefono telefono = new Telefono(rsTelefono.getInt("id"), rsTelefono.getInt("amount"),
-						rsTelefono.getString("description"));
-				ResultSet rsShoppingBasket = conexionDos
-						.query("SELECT * FROM Shopping_Basket WHERE id=" + rsTelefono.getInt("shopping_basket_id"));
-
-				if (rsShoppingBasket != null && rsShoppingBasket.next()) {
-					Calendar calendar = new GregorianCalendar();
-					calendar.setTime(rsShoppingBasket.getDate("date"));
-					ShoppingBasket shoppingBasket = new ShoppingBasket(rsShoppingBasket.getInt("id"), calendar);
-					product.setShoppingBasket(shoppingBasket);
-				}
-				list.add(telefono);
-			}
-
-		} catch (SQLException e) {
-			System.out.println(">>>WARNING (JDBCProductDAO:find): " + e.getMessage());
-		}
-
-		return list;
-	}
-
-
-	@Override
-	public Set<Telefono> findByUserId(String cedulaUser) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Telefono read(String userID) {
+	public Telefono read(String numero) {
 		// TODO Auto-generated method stub
 		Telefono telefono = null;
-		ResultSet rsTelefono = conexionUno.query("SELECT * FROM Telefono WHERE id=" + userID);
+		ResultSet rsTelefono = conexionUno.query("SELECT * FROM Telefono WHERE tel_numero=" + numero);
 		try {
 			if (rsTelefono != null && rsTelefono.next()) {
-				telefono = new Telefono(rsTelefono.getInt("userID"), rsTelefono.getInt("amount"), rsTelefono.getString("description"));
-				ResultSet rsShoppingBasket = conexionDos
-						.query("SELECT * FROM Shopping_Basket WHERE id=" + rsTelefono.getInt("shopping_basket_id"));
+				telefono = new Telefono(rsTelefono.getInt("tel_codigo"), rsTelefono.getString("tel_numero"), rsTelefono.getString("tel_tipo"), rsTelefono.getString("tel_operadora"));
+				ResultSet rsUsuario=conexionDos.query("SELECT * FROM usario WHERE usu_cedula='"+rsTelefono.getString("usu_cedula")+"');");
+				if (rsUsuario !=null && rsUsuario.next()) {
+					Usuario usuario = new Usuario();
+					usuario.setCedula(rsUsuario.getString("usu_cedula"));
+					usuario.setNombre(rsUsuario.getString("usu_nombre"));
+					usuario.setApellido(rsUsuario.getString("usu_apellido"));
+					usuario.setCorreo(rsUsuario.getString("usu_correo"));
+					usuario.setPassword(rsUsuario.getString("usu_contrasena"));
+					telefono.setUsuario(usuario);
+				}
 
 			}
 		} catch (SQLException e) {
@@ -100,12 +69,109 @@ public class JDBCTelefonoDAO extends JDBCGenericDAO<Telefono, String> implements
 		}
 		return telefono;
 	}
-
+	
 	@Override
-	public Set<Telefono> findByEmailId(String correo) {
+	public void update(Telefono telefono) {
 		// TODO Auto-generated method stub
-		return null;
+			conexionUno.update("UPDATE telefono SET tel_numero = '" + telefono.getNumero() + "', tel_tipo = '" + telefono.getTipo()+ "',"
+					+ " tel_operadora= '" +telefono.getOperadora()+"' WHERE tel_codigo = " + telefono.getId());
 	}
 
+	/**
+	 * Se eliminara un telefono especificando el numero de cedula del usuario
+	 */
+	@Override
+	public void delete(Telefono telefono) {
+		// TODO Auto-generated method stub
+		conexionUno.update("DELETE FROM telefono  WHERE tel_codigo = " + telefono.getId());
+	}
+
+	/**
+	 * Busca el telefono por la cedula del usuario
+	 * 
+	 **/
+	@Override
+	public Set<Telefono> findByUserId(String cedulaUser) {
+		// TODO Auto-generated method stub
+		Set<Telefono> list = new HashSet<Telefono>();
+		ResultSet rsTelefono = conexionDos.query("SELECT * FROM telefono WHERE usu_cedula='" + cedulaUser + "';");
+		try {
+			while (rsTelefono.next()) {
+				Telefono telefono = new Telefono(rsTelefono.getInt("tel_codigo"),rsTelefono.getString("tel_numero"), rsTelefono.getString("tel_tipo"),
+						rsTelefono.getString("tel_operadora"));
+//				
+//				ResultSet rsUsuario = conexionDos.query("SELECT * FROM usuario WHERE usu_cedula='" + rsTelefono.getString("usu_cedula") + "';");
+//				if (rsUsuario != null && rsUsuario.next()) {
+//					Usuario usuario = new Usuario();
+//					usuario.setCedula(rsUsuario.getString("usu_cedula"));
+//					usuario.setNombre(rsUsuario.getString("usu_nombre"));
+//					usuario.setApellido(rsUsuario.getString("usu_apellido"));
+//					usuario.setCorreo(rsUsuario.getString("usu_correo"));
+//					usuario.setContrasenia(rsUsuario.getString("usu_contrasenia"));
+//					telefono.setUsuario(usuario);
+//
+//				}
+				list.add(telefono);
+			}
+		} catch (SQLException e) {
+			System.out.println("ERROR JDBCTelefonoDAO en el metodo findbyPersonaCed "+ e.getMessage());
+		}
+
+		return list;
+	}
+
+	/**
+	 * Busca todos los telefonos de la Base de Datos
+	 */
+	@Override
+	public List<Telefono> find() {
+		List<Telefono> list=new ArrayList<Telefono>();
+		ResultSet rsTelefono = conexionUno.query("SELECT * FROM telefono;");
+		try {
+			while (rsTelefono.next()) {
+				Telefono telefono = new Telefono(rsTelefono.getInt("tel_codigo"),rsTelefono.getString("tel_numero"), rsTelefono.getString("tel_tipo"),
+						rsTelefono.getString("tel_operadora"));
+				ResultSet rsUsuario = conexionDos.query("SELECT * FROM usuario WHERE usu_cedula='" + rsTelefono.getString("usu_cedula") + "';");
+				if (rsUsuario != null && rsUsuario.next()) {
+					Usuario usuario = new Usuario();
+					usuario.setCedula(rsUsuario.getString("usu_cedula"));
+					usuario.setNombre(rsUsuario.getString("usu_nombre"));
+					usuario.setApellido(rsUsuario.getString("usu_apellido"));
+					usuario.setCorreo(rsUsuario.getString("usu_correo"));
+					usuario.setPassword(rsUsuario.getString("usu_contrasena"));
+					telefono.setUsuario(usuario);
+
+				}
+				list.add(telefono);
+			}
+		} catch (SQLException e) {
+			System.out.println("ERROR JDBCTelefonoDAO en el metodo List<Telefono> find() "+e.getMessage());
+		}
+		return list;
+	}
+	
+	/**
+	 * Busca el telefono por id
+	 **/
+	@Override
+	public Telefono findbyTelefonoId(int tel_codigo) {
+		// TODO Auto-generated method stub
+		Telefono telefono = null;
+		ResultSet rsTelefono = conexionUno.query("SELECT * FROM telefono WHERE tel_codigo=" + tel_codigo + ";");
+		try {
+			if (rsTelefono != null && rsTelefono.next()) {
+				telefono = new Telefono(rsTelefono.getInt("tel_codigo"), rsTelefono.getString("tel_numero"),
+						rsTelefono.getString("tel_tipo"), rsTelefono.getString("tel_operadora"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return telefono;
+	}
+
+	
+	
+	
 	
 }
